5cac395f9ea180b00b3462f9b14a395cbacf42a8
Lammen1 <fatima.jasem05@gmail.com>
2025-03-23 06:20:36 +0100

    scripts.js

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Funktion som ändrar texten när knappen klickas
+function changeText() {
+    document.querySelector("p").innerText = "Du klickade på knappen!"; 
+}
+console.log("JavaScript-filen är laddad!");
